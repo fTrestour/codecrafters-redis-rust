365cff18e4023487776715385ecf7ac6e38a0515
Fabien <ftrestour@gmail.com>
2023-05-08 12:32:21 +0200

    stage 3

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,38 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").expect("Could not bind to port 6379");
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
-                println!("Accepted new connection");
-
-                _stream.write_all(b"+PONG\r\n").expect("Could not respond");
-            }
+            Ok(stream) => handle_connection(stream),
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    println!("Accepted new connection");
+
+    let mut buf = [0; 512];
+
+    loop {
+        let bytes_read = stream
+            .read(&mut buf)
+            .expect("Could not read bytes from connection");
+
+        if bytes_read == 0 {
+            println!("Closing connection");
+            break;
+        }
+
+        stream.write_all(b"+PONG\r\n").expect("Could not respond")
     }
 }
