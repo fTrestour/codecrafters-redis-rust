b5022c8a1ff6137dfe020b89492dfbb574c270d1
Fabien <ftrestour@gmail.com>
2023-05-08 11:39:36 +0200

    Test PING

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -7,8 +7,10 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut _stream) => {
+                println!("Accepted new connection");
+
+                _stream.write_all(b"+PONG\r\n").expect("Could not respond");
             }
             Err(e) => {
                 println!("error: {}", e);
