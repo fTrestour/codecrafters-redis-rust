4dbb07f573bfeabb432a380ef66716085791cdbe
Fabien <ftrestour@gmail.com>
2023-06-04 10:55:58 +0200

    Refacto + start adding XP

--- src/command.rs
+++ src/command.rs
@@ -1,80 +1,71 @@
-use crate::protocol::Resp;
+use std::time::{Duration, Instant};
+
+use crate::{expiry::Expiry, protocol::Resp};
 
 #[derive(Debug)]
 pub enum Command {
     Ping,
     Echo(String),
-    Set(String, String),
+    Set(String, String, Expiry),
     Get(String),
 }
 
 impl Command {
     pub fn from_resp(message: Resp) -> Option<Command> {
         match message {
-            Resp::SimpleString(s) => {
-                if s.eq("PING") {
-                    return Some(Command::Ping);
-                } else {
-                    return None;
-                }
-            }
-            Resp::BulkString(s) => {
-                if s.eq("PING") {
-                    return Some(Command::Ping);
-                } else {
-                    return None;
-                }
-            }
-            Resp::Array(a) => {
-                let first = a.get(0);
-                match first {
-                    Some(Resp::BulkString(s)) => {
-                        if s.eq("PING") {
-                            return Some(Command::Ping);
-                        } else if s.eq(&"ECHO") {
-                            let second = a.get(1);
-                            match second {
-                                Some(Resp::SimpleString(s)) => {
-                                    return Some(Command::Echo(String::from(s)))
-                                }
-                                Some(Resp::BulkString(s)) => {
-                                    return Some(Command::Echo(String::from(s)))
-                                }
-                                _ => return None,
-                            };
-                        } else if s.eq(&"GET") {
-                            let second = a.get(1);
-                            match second {
-                                Some(Resp::SimpleString(s)) => {
-                                    return Some(Command::Get(String::from(s)))
-                                }
-                                Some(Resp::BulkString(s)) => {
-                                    return Some(Command::Get(String::from(s)))
-                                }
-                                _ => return None,
-                            };
-                        } else if s.eq(&"SET") {
-                            let second = a.get(1);
-                            let third = a.get(2);
-                            match (second, third) {
-                                (Some(Resp::SimpleString(s)), Some(Resp::SimpleString(t))) => {
-                                    return Some(Command::Set(String::from(s), String::from(t)))
-                                }
-                                (Some(Resp::SimpleString(s)), Some(Resp::BulkString(t))) => {
-                                    return Some(Command::Set(String::from(s), String::from(t)))
-                                }
-                                (Some(Resp::BulkString(s)), Some(Resp::BulkString(t))) => {
-                                    return Some(Command::Set(String::from(s), String::from(t)))
-                                }
-                                _ => return None,
-                            };
-                        } else {
-                            return None;
-                        }
+            Resp::SimpleString(s) | Resp::BulkString(s) if s.eq("PING") => Some(Command::Ping),
+
+            Resp::Array(a) => match a.get(0)? {
+                Resp::SimpleString(s) | Resp::BulkString(s) if s.eq("PING") => Some(Command::Ping),
+
+                Resp::BulkString(s) if s.eq(&"ECHO") => match a.get(1)? {
+                    Resp::SimpleString(s) | Resp::BulkString(s) => {
+                        Some(Command::Echo(String::from(s)))
                     }
-                    _ => return None,
-                }
-            }
-        };
+                    _ => None,
+                },
+
+                Resp::BulkString(s) if s.eq(&"GET") => match a.get(1)? {
+                    Resp::SimpleString(s) | Resp::BulkString(s) => Some(Command::Get(s.clone())),
+                    _ => None,
+                },
+
+                Resp::BulkString(s) if s.eq(&"SET") => match a.get(1)? {
+                    Resp::SimpleString(k) | Resp::BulkString(k) => match a.get(2)? {
+                        Resp::SimpleString(v) | Resp::BulkString(v) => match a.get(3) {
+                            None => Some(Command::Set(
+                                String::from(k),
+                                String::from(v),
+                                Expiry::Infinity,
+                            )),
+
+                            Some(Resp::SimpleString(s)) | Some(Resp::BulkString(s))
+                                if s.eq("PX") =>
+                            {
+                                match a.get(4)? {
+                                    Resp::SimpleString(d) | Resp::BulkString(d) => {
+                                        let d = d.parse::<u64>().ok()?;
+                                        return Some(Command::Set(
+                                            String::from(k),
+                                            String::from(v),
+                                            Expiry::ExpiresAt(
+                                                Instant::now() + Duration::from_millis(d),
+                                            ),
+                                        ));
+                                    }
+                                    _ => None,
+                                }
+                            }
+
+                            _ => None,
+                        },
+                        _ => None,
+                    },
+                    _ => None,
+                },
+                _ => None,
+            },
+            _ => None,
+        }
     }
 }
